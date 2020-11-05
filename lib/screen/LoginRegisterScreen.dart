ba8635491d4e30ffe428bc92ba7cd37c0985feae
Sergio <sergio.casero23@gmail.com>
2020-11-05 21:32:00 +0100

    Created Login and Home screens

---
+++ lib/screen/LoginRegisterScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginRegisterScreen extends StatefulWidget {
+  @override
+  _LoginRegisterScreenState createState() => _LoginRegisterScreenState();
+}
+
+class _LoginRegisterScreenState extends State<LoginRegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
